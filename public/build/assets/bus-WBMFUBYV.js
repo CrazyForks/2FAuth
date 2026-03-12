6fbaa16f44be0fe03f44b6995d0c55699809c0f3
Bubka <858858+Bubka@users.noreply.github.com>
2026-03-12 21:28:57 +0100

    Rebuild assets

--- public/build/assets/bus-WBMFUBYV.js
+++
@@ -1,1 +0,0 @@
-import{ae as e}from"./app-DW8SEQmM.js";const r=e("bus",{state:()=>({migrationUri:null,decodedUri:null,inManagementMode:!1,editedGroupName:null,username:null}),actions:{}});export{r as u};
